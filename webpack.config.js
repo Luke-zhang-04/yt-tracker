a0ef35a2f6626e45ec55536b15ece7e64c3f7dd3
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-08-23 14:58:29 -0400

    initialize popup js
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,25 @@
 
 const path = require("path")
 
-module.exports = {
-    entry: "./lib/index.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "tracker.js",
-        library: "tracker",
-        libraryTarget: "umd",
+module.exports = [
+    {
+        entry: "./lib/index.js",
+        output: {
+            path: path.resolve(__dirname, "dist"),
+            filename: "tracker.js",
+            library: "tracker",
+            libraryTarget: "umd",
+        },
+        mode: "production",
     },
-    mode: "production",
-}
+    {
+        entry: "./ui/lib/index.js",
+        output: {
+            path: path.resolve(__dirname, "ui/js"),
+            filename: "script.js",
+            library: "trackerUI",
+            libraryTarget: "umd",
+        },
+        mode: "production",
+    },
+]
